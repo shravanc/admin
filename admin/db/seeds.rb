482ac5d5b07b7f739041132a2fca7299041699db
shravanc <shravan007.c@gmail.com>
2019-11-30 13:50:19 +0000

    see changes

--- admin/db/seeds.rb
+++ admin/db/seeds.rb
@@ -5,3 +5,57 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Apartment:Switch!('amazon')
+user = User.where(username: "admin", firstname: "admin", password: "password").first_or_create
+admin = Role.where(title: "admin").first_or_create
+
+privileges = ['create_items', 'update_items', 'destroy_items', 'create_lists', 'destroy_lists', 'create_layouts', 'update_layouts', 'create_media', 'update_media']
+privileges.each do |p1|
+  Privilege.where(title: p1).first_or_create
+end
+
+admin.privileges << Privilege.all
+
+
+list_operation = Role.where(title: 'list_operation').first_or_create
+list_operation.privileges << Privilege.find_by(title: 'create_lists')
+list_operation.privileges << Privilege.find_by(title: 'destroy_lists')
+
+
+item_operation = Role.where(title: 'item_operation').first_or_create
+item_operation.privileges << Privilege.find_by(title:'create_items')
+item_operation.privileges << Privilege.find_by(title:'update_items')
+item_operation.privileges << Privilege.find_by(title:'destroy_items')
+
+
+user.role = admin
+
+
+
+#Netflix ontent
+Apartment:Switch!('netflix')
+user = User.where(username: "admin", firstname: "admin", password: "password").first_or_create
+admin = Role.where(title: "admin").first_or_create
+
+privileges = ['create_items', 'update_items', 'destroy_items', 'create_lists', 'destroy_lists', 'create_layouts', 'update_layouts', 'create_media', 'update_media']
+privileges.each do |p1|
+  Privilege.where(title: p1).first_or_create
+end
+
+admin.privileges << Privilege.all
+
+
+list_operation = Role.where(title: 'list_operation').first_or_create
+list_operation.privileges << Privilege.find_by(title: 'create_lists')
+list_operation.privileges << Privilege.find_by(title: 'destroy_lists')
+
+
+item_operation = Role.where(title: 'item_operation').first_or_create
+item_operation.privileges << Privilege.find_by(title:'create_items')
+item_operation.privileges << Privilege.find_by(title:'update_items')
+item_operation.privileges << Privilege.find_by(title:'destroy_items')
+
+
+user.role = admin
+
---
+++ admin/lib/tasks/initial_setup.rb
@@ -0,0 +1,29 @@
+desc "Setting up Privileges and Admin Roles"
+task "initial_setup" do
+
+  Tenant.switch!("amazon")
+
+  user = User.where(username: "admin", firstname: "admin", password: "password").first_or_create
+  admin = Role.where(title: "admin").first_or_create
+
+  privileges = ['create_items', 'update_items', 'destroy_items', 'create_lists', 'destroy_lists', 'create_layouts', 'update_layouts', 'create_media', 'update_media']
+  privileges.each do |p1|
+    Privilege.where(title: p1).first_or_create
+  end
+
+  admin.privileges << Privilege.all
+
+
+  list_operation = Role.where(title: 'list_operation').first_or_create
+  list_operation.privileges << Privilege.find_by(title: 'create_lists')
+  list_operation.privileges << Privilege.find_by(title: 'destroy_lists')
+
+
+  item_operation = Role.where(title: 'item_operation').first_or_create
+  item_operation.privileges << Privilege.find_by(title:'create_items')
+  item_operation.privileges << Privilege.find_by(title:'update_items')
+  item_operation.privileges << Privilege.find_by(title:'destroy_items')
+
+  user.role = admin
+
+end
