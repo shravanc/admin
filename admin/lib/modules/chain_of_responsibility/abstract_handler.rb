241b84706d8e28976e126db47a9a6194710aa7c9
Reezvee Sikder <rezvee6@gmail.com>
2019-12-10 10:55:57 +0000

    Added comments

--- admin/lib/modules/chain_of_responsibility/abstract_handler.rb
+++ admin/lib/modules/chain_of_responsibility/abstract_handler.rb
@@ -1,12 +1,13 @@
 class AbstractHandler < Handler
   attr_writer :next_handler
 
-
+#This method is caled while resgistering the responsibility and will set the instance variable @next_handler to next responsibility class.
   def next_handler(handler)
     @next_handler = handler
-    handler
+    retun handler
   end
 
+  #this function will be called when any rensponsibility failed to serve the request.
   def handle 
     return @next_handler.handle(request) if @next_handler
 
