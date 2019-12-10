aa2a27ae2d9cc3f00b965f4380f9d27d48b5bc85
Reezvee Sikder <rezvee6@gmail.com>
2019-12-10 10:56:18 +0000

    Added comments

--- admin/lib/modules/chain_of_responsibility/encrypt_password.rb
+++ admin/lib/modules/chain_of_responsibility/encrypt_password.rb
@@ -1,5 +1,7 @@
 class EncryptPassword < AbstractHandler
 
+#If the handler cannot handle the request or if it is not its request to serve, it passes the control to parent and then parent 
+#passes the control to the respective responsibilty class in the order of how the responsibility are registered.
 def handle()
   if request == "encrypt"
     put "encrypted"
